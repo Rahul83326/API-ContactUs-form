98b1bd081567e5ddaa31667659fc929b5f6b6ce5
RAHUL THANGAMANI <82194182+Rahul83326@users.noreply.github.com>
2022-02-09 10:13:02 -0800

    Update db.config.js

--- config/db.config.js
+++ config/db.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url: "mongodb+srv://RahulThangamani:rahulT@83326@cluster0.jh8qn.mongodb.net/ContactUs?retryWrites=true&w=majority"
+    url: "mongodb+srv://RahulThangamani:<password>@cluster0.jh8qn.mongodb.net/<projectname>?retryWrites=true&w=majority"
 }
