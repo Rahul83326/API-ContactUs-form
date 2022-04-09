bebc45cffcbc848c6274f19fe894dc5fd14a995a
RAHUL THANGAMANI <82194182+Rahul83326@users.noreply.github.com>
2022-04-09 11:35:27 +0530

    Update user.model.js

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,13 +1,85 @@
 const mongoose = require('mongoose');
 
 const UserSchema = mongoose.Schema({
-    subject: String,
-    message: String,
-    is_active:  { type: Boolean, default: false },
-    is_verified:  { type: Boolean, default: false },
-    is_deleted:  { type: Boolean, default: false }
-}, {
-    timestamps: true
-});
+      academic: {
+        type: String,
+        required: true
+      },
+      year: {
+        type: String,
+        required: true
+      },
+      semester: {
+        type: String,
+        required: true
+      },
+      branch: {
+        type: String,
+        required: true
+      },
+      course: {
+        type: String,
+        required: true
+      },
+      name: {
+        type: String,
+        required: true
+      },
+      community_admitted: {
+        type: String,
+        required: true
+      },
+      community_student: {
+        type: String,
+        required: true
+      },
+      mode: {
+        type: String,
+        required: true
+      },
+      score: {
+        type: String,
+        required: true
+      },
+      account: {
+        type: String,
+        required: true
+      },
+      bank: {
+        type: String,
+        required: true
+      },
+      ifsc: {
+        type: String,
+        required: true
+      },
+      phonenumber: {
+        type: String,
+        required: true
+      },
+      email: {
+        type: String,
+        required: true
+      },
+      arrear: {
+        type: String,
+        required: true
+      },
+      attendance: {
+        type: String,
+        required: true
+      },
+      remark: {
+        type: String,
+        required: true
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now()
+      }
+      
+      });
+
+
 
 module.exports = mongoose.model('User', UserSchema);
